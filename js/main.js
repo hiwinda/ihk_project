c5f8a2ef37d0a4e1c1f0502669cc227124591356
Winda Pra <57739210+hiwinda@users.noreply.github.com>
2021-05-19 10:22:05 +0700

    slide on page: gallery

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
-$(document).ready(function(){
-    /*--- HEADLINE SLICK ---*/ 
+$(document).ready(function () {
+	/*--- HEADLINE SLICK ---*/
 	$('.ihk-headline__slide').slick({
 		arrows: false,
 		dots: false,
 		slidesToShow: 1
 	});
-	/*--- PUBLICATION SLICK ---*/ 
+	/*--- GALLERY SLICK ---*/
+	$('.gallery-main').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		fade: true,
+		asNavFor: '.gallery-nav'
+	});
+	$('.gallery-nav').slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		asNavFor: '.gallery-main',
+		focusOnSelect: true
+	});
+	/*--- PUBLICATION SLICK ---*/
 	$('.ihk-publication__slide').slick({
 		dots: false,
 		slidesToShow: 1
