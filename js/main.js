c775da926c7c32ba6480ad28d18aef4754b5ad57
Winda Pra <57739210+hiwinda@users.noreply.github.com>
2021-05-19 09:35:47 +0700

    all page

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
-    /*--- Start:: Slick ---*/ 
+    /*--- HEADLINE SLICK ---*/ 
 	$('.ihk-headline__slide').slick({
 		arrows: false,
+		dots: false,
+		slidesToShow: 1
+	});
+	/*--- PUBLICATION SLICK ---*/ 
+	$('.ihk-publication__slide').slick({
 		dots: false,
 		slidesToShow: 1
 	});
